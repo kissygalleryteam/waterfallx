c0357bbc97fc2c16dfb1882a5f15a3f3dcc97866
弘树 <tiehang.lth@alibaba-inc.com>
2014-07-31 21:24:08 +0800

    sync daily version 2.0.0

--- 2.0.0/index.js
+++ 2.0.0/index.js
@@ -1,7 +1,29 @@
-;
-KISSY.add(function (S, WaterfallX, Loader) {
-    WaterfallX.Loader = Loader
-    return WaterfallX
-}, {
-    requires:['./base', './loader']
-})
+/**
+ * @fileoverview
+ * @author
+ * @module waterfallx
+ **/
+KISSY.add(function (S, Node,Base) {
+    var EMPTY = '';
+    var $ = Node.all;
+    /**
+     *
+     * @class Waterfallx
+     * @constructor
+     * @extends Base
+     */
+    function Waterfallx(comConfig) {
+        var self = this;
+        //调用父类构造函数
+        Waterfallx.superclass.constructor.call(self, comConfig);
+    }
+    S.extend(Waterfallx, Base, /** @lends Waterfallx.prototype*/{
+
+    }, {ATTRS : /** @lends Waterfallx*/{
+
+    }});
+    return Waterfallx;
+}, {requires:['node', 'base']});
+
+
+
